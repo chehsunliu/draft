6577a3610049a1e05a987cfb78bfa5140fc80dab
Che-Hsun Liu <chehsunliu@gmail.com>
2026-05-02 00:44:26 +0800

    Add basic code for list-posts api

--- itx-rs/itx-backend/src/feature/post/mod.rs
+++ itx-rs/itx-backend/src/feature/post/mod.rs
@@ -2,18 +2,22 @@ use crate::error::BackendError;
 use crate::feature::post::use_case::list_posts;
 use crate::middleware::context::ItxContext;
 use crate::state::AppState;
+use axum::extract::State;
 use axum::routing::get;
 use axum::{Extension, Json, Router};
+use itx_contract::repo::post::PostRepo;
+use std::sync::Arc;
 
 pub mod use_case;
 
 async fn list_posts(
+    State(post_repo): State<Arc<dyn PostRepo>>,
     Extension(context): Extension<ItxContext>,
 ) -> Result<Json<list_posts::ExecuteOutput>, BackendError> {
     let params = list_posts::ExecuteParams {
         user_id: context.user_id.unwrap(),
     };
-    let use_case = list_posts::ListPostsUseCase::new();
+    let use_case = list_posts::ListPostsUseCase::new(post_repo);
     Ok(Json(use_case.execute(params).await?))
 }
 
--- itx-rs/itx-backend/src/feature/post/use_case/list_posts.rs
+++ itx-rs/itx-backend/src/feature/post/use_case/list_posts.rs
@@ -1,5 +1,7 @@
 use crate::error::BackendError;
+use itx_contract::repo::post::PostRepo;
 use serde::Serialize;
+use std::sync::Arc;
 use uuid::Uuid;
 
 pub struct ExecuteParams {
@@ -16,11 +18,13 @@ pub struct ExecuteOutput {
 #[serde(rename_all = "camelCase")]
 pub struct Item {}
 
-pub struct ListPostsUseCase {}
+pub struct ListPostsUseCase {
+    post_repo: Arc<dyn PostRepo>,
+}
 
 impl ListPostsUseCase {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(post_repo: Arc<dyn PostRepo>) -> Self {
+        Self { post_repo }
     }
 
     pub async fn execute(&self, params: ExecuteParams) -> Result<ExecuteOutput, BackendError> {
